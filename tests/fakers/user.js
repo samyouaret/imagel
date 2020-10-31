00958962653f9dbb69173237acce2895f305b786
samyouaret <samyouaret13@gmail.com>
2020-10-31 19:54:04 +0100

    stop tracking public assets

--- tests/fakers/user.js
+++ tests/fakers/user.js
@@ -2,8 +2,8 @@ module.exports = {
     id: 1,
     firstName: 'James',
     lastName: 'Doe',
-    email: 'james@doe.com',
-    password: 'James123456',
+    email: 'joe@doe.com',
+    password: 'Joe123456',
     createdAt: new Date(),
     updatedAt: new Date()
 }
